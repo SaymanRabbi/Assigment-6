f2247a6272cdffb0eba6f189c567c5e5018d88fc
Sayman Rabbi <mdrabbi.asm@gmail.com>
2021-09-25 21:36:30 +0600

    4th

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// problem solve 1
+
+const gfNaiAmr = function (mobile,laptop,watch){
+    document.write(`Mobile Prices =${mobile} Tk🤑`+ "<br>")
+    document.write(`Laptop Prices =${laptop} Tk🤑`+ "<br>")
+    document.write(`Watch Prices =${watch} Tk🤑`+ "<br>")
+    
+    const total = mobile+laptop+watch;
+    return total;
+}
+const total4=gfNaiAmr(5000,2000,2000);
+document.write(`Your Total Bill Was = ${total4} Tk💸💸` + "<br>" + "<br>");
+// -----end---
+
+// problem solve 2
+const billCheck = function (day) {
+    if (10>=day){
+        const sum1= day*100;
+        document.write(`Your Bil was ${sum1} Tk💸💸` + "<br>");
+    }
+    
+    if (11<=day && 20>=day){
+        const sum2 = ((day-10)*80) + 1000;
+        document.write(`Your Bill Was ${sum2} Tk💸💸` + "<br>")
+    }
+    
+    if (21<=day){
+        const sum3 = ((day-20)*50) + 1800;
+        document.write(`Your Bill Was ${sum3} Tk💸💸` + "<br>" + "<br>")
+    }
+}
+
+billCheck(21);
+
+// --end--
 function longestWord(array) {
     let longest = "";
    array.map(function (str) {
@@ -9,34 +44,3 @@ return longest;
  }
  const bigName=(longestWord(["KeepRolling","Rabbi","Amrnamholohabu"]));
  document.write(`Its A Big Name From This Array "${bigName}"😊😊;` + "<br>" + "<br>")
-// problem solve 2
-
-const gfNaiAmr = function (mobile,laptop,watch){
-const total = mobile+laptop+watch;
-return total;
-}
-const total4=gfNaiAmr(5000,2000,2000);
-document.write(`Your Bill Was ${total4} Tk💸💸` + "<br>" + "<br>");
-// -----end---
-
-// problem solve 3 
-const billCheck = function (day) {
-if (10>=day){
-const sum1= day*100;
-document.write(`Your Bil was ${sum1} Tk💸💸` + "<br>");
-    }
-
-if (11<=day && 20>=day){
-const sum2 = ((day-10)*80) + 1000;
-document.write(`Your Bill Was ${sum2} Tk💸💸` + "<br>")
-}
-
-if (21<=day){
-const sum3 = ((day-20)*50) + 1800;
-document.write(`Your Bill Was ${sum3} Tk💸💸` + "<br>")
-}
-}
-    
-billCheck(21);
-
-// --end--
